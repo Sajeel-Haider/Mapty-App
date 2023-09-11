201a66c4ac9804fb15d3fe6130b9f51fc171a969
Sajeel Haider <90964891+Sajeel-Haider@users.noreply.github.com>
2023-09-11 22:11:47 +0500

    Mapty App

---
+++ assignments.js
@@ -0,0 +1,13 @@
+// //Part1
+// let country;
+// let continent;
+// let population;
+// country = 'Pakistan';
+// continent = 'Asia';
+// population = '1 Billion';
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// //Part2
+// let language = 'Urdu';
+// const age = 17;
